d552f841f825d1f9bcfe99232f1493825a7df18e
Ryan Morrison <ryan.morrison04@icloud.com>
2022-04-09 13:45:52 -0400

    Initial commit

---
+++ src/main/java/net/ryanmorrison/chatterbox/ChatterboxApplication.java
@@ -0,0 +1,13 @@
+package net.ryanmorrison.chatterbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterboxApplication.class, args);
+    }
+
+}
