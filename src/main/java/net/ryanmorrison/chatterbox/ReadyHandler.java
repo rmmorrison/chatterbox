b054381f4b4bb4e5023832dbccc694cdf0e8415d
Ryan Morrison <ryan.morrison04@icloud.com>
2022-04-09 22:36:58 -0400

    Rewrite slash command registration to complete in single REST request
    Avoids being rate limited by the Discord API.

--- src/main/java/net/ryanmorrison/chatterbox/ReadyHandler.java
+++ src/main/java/net/ryanmorrison/chatterbox/ReadyHandler.java
@@ -1,15 +1,18 @@
 package net.ryanmorrison.chatterbox;
 
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.ryanmorrison.chatterbox.framework.SlashCommandsListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
+import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -24,32 +27,36 @@ public class ReadyHandler extends ListenerAdapter {
 
     @Override
     public void onReady(@NotNull ReadyEvent event) {
-        listeners.stream()
+        Collection<SlashCommandData> combinedCommands = listeners.stream()
                 .filter(listener -> listener instanceof SlashCommandsListenerAdapter)
                 .map(listener -> (SlashCommandsListenerAdapter) listener)
-                .forEach(listener -> {
-                    if (listener.getSupportedCommands().isEmpty()) {
-                        log.warn("Listener {} has no commands to register even though it extends the " +
-                                "SlashCommandsListenerAdapter abstract class. This probably isn't desired.",
-                                listener.getClass().getName());
-                        return; // continue as there's nothing we can do with this listener
-                    }
+                .flatMap(listener -> listener.getSupportedCommands().stream())
+                .toList();
 
-                    if (isDebugModeEnabled()) {
-                        event.getJDA().getGuilds().forEach(guild -> {
-                            log.info("Listener {} registering {} commands in server \"{}\" (debug mode).",
-                                    listener.getClass().getName(), listener.getSupportedCommands().size(),
-                                    guild.getName());
-                            guild.updateCommands()
-                                    .addCommands(listener.getSupportedCommands().toArray(SlashCommandData[]::new)).queue();
-                        });
-                    }
-                    else {
-                        log.info("Listener {} registering {} commands globally.",
-                                listener.getClass().getName(), listener.getSupportedCommands().size());
-                        listener.getSupportedCommands().forEach(command -> event.getJDA().upsertCommand(command).queue());
-                    }
-                });
+        if (isDebugModeEnabled()) {
+            CommandListUpdateAction combinedActions = null;
+            for (Guild guild : event.getJDA().getGuilds()) {
+                log.info("Registering {} total commands in server \"{}\" (debug mode).", combinedCommands.size(),
+                        guild.getName());
+                if (combinedActions == null) {
+                    combinedActions = guild.updateCommands().addCommands(combinedCommands.toArray(SlashCommandData[]::new));
+                } else {
+                    combinedActions.zip(guild.updateCommands().addCommands(combinedCommands.toArray(SlashCommandData[]::new)));
+                }
+            }
+
+            if (combinedActions != null) {
+                combinedActions.queue();
+            }
+            else
+            {
+                log.warn("No guilds were discovered to register commands to (debug mode). Nothing to do.");
+            }
+        }
+        else {
+            log.info("Registering {} commands globally.", combinedCommands.size());
+            event.getJDA().updateCommands().addCommands(combinedCommands.toArray(SlashCommandData[]::new)).queue();
+        }
     }
 
     private boolean isDebugModeEnabled() {
