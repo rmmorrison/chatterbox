7e4cc019a10ee3250ed57850a4fea77ebff9b189
Ryan Morrison <ryan.morrison04@icloud.com>
2022-04-14 16:20:11 -0400

    Add missing repository annotations

--- src/main/java/net/ryanmorrison/chatterbox/persistence/repository/CopypastaRepository.java
+++ src/main/java/net/ryanmorrison/chatterbox/persistence/repository/CopypastaRepository.java
@@ -2,9 +2,11 @@ package net.ryanmorrison.chatterbox.persistence.repository;
 
 import net.ryanmorrison.chatterbox.persistence.model.CopypastaEntry;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CopypastaRepository extends CrudRepository<CopypastaEntry, Integer> {
 
     List<CopypastaEntry> findAllByChannelId(long channelId);
--- src/main/java/net/ryanmorrison/chatterbox/persistence/repository/ShoutHistoryRepository.java
+++ src/main/java/net/ryanmorrison/chatterbox/persistence/repository/ShoutHistoryRepository.java
@@ -2,7 +2,9 @@ package net.ryanmorrison.chatterbox.persistence.repository;
 
 import net.ryanmorrison.chatterbox.persistence.model.ShoutHistory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShoutHistoryRepository extends CrudRepository<ShoutHistory, Long> {
 
     ShoutHistory findShoutHistoryByChannelId(long channelId);
